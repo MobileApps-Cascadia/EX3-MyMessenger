cd8ec14458b121e19a3eb49a06939508de0b559c
Dr. Brian Bansenauer <brianb@cascadia.edu>
2022-10-21 16:17:49 -0700

    Initial commit

---
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.kt
@@ -0,0 +1,51 @@
+package com.hfad.mymessenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.CheckedTextView
+import com.hfad.mymessenger.databinding.ActivityCreateMessageBinding
+
+
+class CreateMessageActivity : AppCompatActivity() {
+    //Declare the binding variable as a private lateinit var
+    private lateinit var binding: ActivityCreateMessageBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCreateMessageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //TODO: Add a setOnClickListener to each Button using the binding
+
+    }
+
+    //Creates an Explicit Intent to start the ReceiveMessageActivity
+    private fun sendMessageToActivity() {
+        //TODO: Create an explicit Intent for ReceiveMessageActivity
+
+        // TODO: use putExtra method to attach the string value from the message editText field
+        //      and the boolean "isChecked" value from the markUrgent checkbox (use the binding to get this)
+
+
+        //  TODO: Start the new Activity
+
+    }
+
+    //Creates an IMPLICIT intent, adds the EditText's message as a String, and sends it to a "Chooser" window for the user to pick
+    fun sendMessageToOtherApp() {
+        val dataIntent = Intent(Intent.ACTION_SEND)
+        dataIntent.type = "text/plain"
+        //TODO: use putExtra method with the label "Intent.EXTRA_TEXT" to attach the user's message to the intent
+        //  and send it to a "Chooser" window for the user to see it with the different apps
+
+    }
+
+    //Reference this data label in the message receiver class using CreateMessageActivity.MESSAGE
+    companion object {
+        const val MESSAGE = "MESSAGE"
+        //TODO: Add a constant, URGENT, representing the data label "URGENT"
+
+    }
+}
---
+++ app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.kt
@@ -0,0 +1,28 @@
+package com.hfad.mymessenger
+
+import android.os.Bundle
+import com.hfad.mymessenger.R
+import android.content.Intent
+import android.view.View
+import com.hfad.mymessenger.CreateMessageActivity
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.hfad.mymessenger.databinding.ActivityReceiveMessageBinding
+
+class ReceiveMessageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityReceiveMessageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityReceiveMessageBinding.inflate(layoutInflater)
+
+        //The Activity property 'intent' represents the particular intent that started this Activity
+        var message = intent?.extras?.getString(CreateMessageActivity.MESSAGE).toString()
+
+        //TODO: Add an exclamation mark to the TextView if the intent boolean data "URGENT" is true
+
+            binding.message.text = message
+
+        setContentView(binding.root) // this is called after the rest of the setup, so that the message will display
+    }
+}
